a2ae2e355c02ec1be4cbd9b1efeff3f8bc7a6f90
amirrahemi01 <amirrahemi01@gmail.com>
2023-10-25 05:27:55 +0330

    Update Error Page

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import IMG from "../Assets/Images/404.png"
+import { Link } from 'react-router-dom'
 
 export default function NotFound() {
   return (
-    <div>
+    <div className='error-container'>
         <img src={IMG} />
-        <p>Error</p>
+        <h2>Sorry</h2>
+        <p>did not find the page.</p>
+        <Link to="/" className='main-btn'>Go To Home</Link>
     </div>
   )
 }
